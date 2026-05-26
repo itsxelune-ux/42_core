3b6d9386a53ad247d65eacdc45a1aaed5f059c24
itsxelune-ux <itsxelune@gmail.com>
2026-05-26 19:21:35 +0200

    nz

---
+++ python-projects/python_piscine/module_6/alchemy/__init__.py
@@ -0,0 +1,7 @@
+from .elements import create_air
+from .elements import create_earth
+
+from .potions import strength_potion
+from .potions import healing_potion as heal
+
+from .transmutation.recipes import lead_to_gold
---
+++ python-projects/python_piscine/module_6/ft_alembic_2.py
@@ -0,0 +1,5 @@
+from alchemy import elements
+
+print("=== Alembic 2 ===")
+print("Accessing alchemy/elements.py using 'import ...' structure")
+print(f"Testing create_earth: {elements.create_earth()}")
