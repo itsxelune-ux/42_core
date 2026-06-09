1ccbb61d375aabf6992e0407122a05819a6fbe45
olgix <itsxelune@gmail.com>
2026-06-09 16:51:22 +0200

    Update python-projects/python_piscine/module_6/ft_alembic_1.py
    Co-authored-by: Marek Macků <83927217+marekmacku@users.noreply.github.com>

--- python-projects/python_piscine/module_6/ft_alembic_1.py
+++ python-projects/python_piscine/module_6/ft_alembic_1.py
@@ -2,4 +2,4 @@ from elements import create_water
 
 print("=== Alembic 1 ===")
 print("Using: 'from ... import ...' structure to acess elements.py")
-print(f"Testing create_water: {elements.create_water()}")
+print(f"Testing create_water: {create_water()}")
