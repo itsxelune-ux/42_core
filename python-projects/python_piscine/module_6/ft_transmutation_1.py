1f0f4a4182e35c3ddca2495ef786848d1548dcd6
olgix <itsxelune@gmail.com>
2026-06-09 16:49:40 +0200

    Update python-projects/python_piscine/module_6/ft_transmutation_1.py
    Co-authored-by: Marek Macků <83927217+marekmacku@users.noreply.github.com>

--- python-projects/python_piscine/module_6/ft_transmutation_1.py
+++ python-projects/python_piscine/module_6/ft_transmutation_1.py
@@ -2,4 +2,4 @@ from alchemy.transmutation import lead_to_gold
 
 print("=== Transmutation 1 ===")
 print("Import transmutation module directly")
-print(f"Testing lead to gold: {lead_to_gold()}")
+print(f"Testing lead to gold: {import alchemy.transmutation.lead_to_gold()}")
