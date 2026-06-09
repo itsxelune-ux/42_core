bebd4f15410561cd634d1ed873566ec53fa5a843
olgix <itsxelune@gmail.com>
2026-06-09 16:50:33 +0200

    Update python-projects/python_piscine/module_6/ft_transmutation_0.py
    Co-authored-by: Marek Macků <83927217+marekmacku@users.noreply.github.com>

--- python-projects/python_piscine/module_6/ft_transmutation_0.py
+++ python-projects/python_piscine/module_6/ft_transmutation_0.py
@@ -2,4 +2,4 @@ from alchemy.transmutation.recipes import lead_to_gold
 
 print("=== Transmutation 0 ===")
 print("Using file  alchemy/transmutation/recipes.py directly")
-print(f"Testing lead to gold: {lead_to_gold()}")
+print(f"Testing lead to gold: { alchemy.transmutation.recipes.lead_to_gold()}")
